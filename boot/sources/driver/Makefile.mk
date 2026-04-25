04c4e05f88735da36fab9d6c0dea8e064f8294bc
leminhduy00 <leminhduy00@gmail.com>
2026-04-25 16:55:19 +0700

    boot: update driver Makefile

--- boot/sources/driver/Makefile.mk
+++ boot/sources/driver/Makefile.mk
@@ -1,11 +1,14 @@
+include sources/driver/gpio/Makefile.mk
+include sources/driver/iwdg/Makefile.mk
+include sources/driver/usart/Makefile.mk
+include sources/driver/timer/Makefile.mk
+include sources/driver/spi/Makefile.mk
+include sources/driver/flash/Makefile.mk
+
 CFLAGS   += -I./sources/driver/led
-CFLAGS   += -I./sources/driver/flash
 
 CPPFLAGS += -I./sources/driver/led
-CPPFLAGS += -I./sources/driver/flash
 
 VPATH += sources/driver/led
-VPATH += sources/driver/flash
 
 SOURCES += sources/driver/led/led.c
-SOURCES += sources/driver/flash/flash.c
